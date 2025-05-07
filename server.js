ffcb5f700b0bfed72a48f0dada63c783bfe2078b
lostinaa <lostinaa@github.com>
2025-05-07 13:40:23 +0000

    minor changes on API, env

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 
+const port = process.env.PORT || 8000;
 const app = express();
 
-//static
-app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(8001, () => console.log("server running"));
+
+// Serve static files
+ //app.use(express.static( path.join( __dirname, 'public' )))
+//app.use(express.static(path.join(__dirname, 'public')));
+ 
+
+let posts = [
+
+    {id:1, title: 'post one'},
+    {id:2 , title: 'post two'},
+    {ide:3, title: 'post three'}
+]
+ app.get('/api/posts' , (req, res)  => {
+res.json(posts);
+ })
+
+
+ 
+
+app.listen(port, () => console.log("server started"))
